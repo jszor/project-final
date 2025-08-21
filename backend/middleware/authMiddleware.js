c250a847f7aaca4220c9dfd57ba9be4f018ab928
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-08-21 12:39:04 +0200

    add authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.js";
+
+export const authenticateUser = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Attach user to request
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: "Not authorized, token failed" });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ error: "Not authorized, no token" });
+  }
+}
