9a602d584a8299302ed221ce4f5629968a238590
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-08-23 10:47:16 +0200

    add petCreate controller function in petController

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -1,9 +1,46 @@
 import { Pet } from "../models/pet.js";
 import { applyPetDecay } from "../utils/petUtils.js";
 
+// POST create new pet
+
+export const createPet = async (req, res) => {
+
+  try {
+
+    const { name } = req.body;
+
+    // Find user's current pet
+    let existingPet = await Pet.findOne({ owner: req.user._id }).sort({ createdAt: -1 });
+
+    if (existingPet) {
+      existingPet = applyPetDecay(existingPet);
+
+      if (existingPet.status === "alive") {
+        return res.status(400).json({ message: "You already have a living pet!" });
+      }
+    }
+
+    // Create new pet
+    const newPet = await Pet.create({
+      owner: req.user._id,
+      name: name || "Pomodoro",
+    });
+
+    res.status(201).json(newPet);
+
+  } catch (error) {
+
+    console.error("Error creating pet:", error);
+
+    res.status(500).json({ message: "Server error" });
+
+  }
+};
+
 // GET current pet
 
 export const getPet = async (req, res) => {
+  
   try {
     // Find pet by user
     let pet = await Pet.findOne({ owner: req.user._id });
@@ -19,8 +56,12 @@ export const getPet = async (req, res) => {
     await pet.save();
 
     res.json(pet);
+
   } catch (error) {
+
     console.error("Error fetching pet:", error);
+    
     res.status(500).json({ message: "Server error" });
+
   }
 };
