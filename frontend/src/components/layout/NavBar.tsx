08d4ce24c4cc13c67dd6c6a05e28365abbed52ce
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-11-09 20:58:04 +0100

    make about lead home for now

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
         <Link to="/" className="focus:underline focus:outline-none focus:ring-0">
           Home
         </Link>
-        <Link to="/about" className="focus:underline focus:outline-none focus:ring-0">
+        <Link to="/" className="focus:underline focus:outline-none focus:ring-0">
           About
         </Link>
         <Link to="/login" className="outline-2 outline-ammo-100 focus:underline rounded-[30px] px-[34px] py-[14px]">
